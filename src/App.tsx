685cfcc9bc9182caf632f0c046376c025c292e45
Arkadiy B <adiy.work.mail@ya.ru>
2023-01-15 14:31:02 +0300

    feat: add tpm layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,23 @@
 const App = () => {
-  return <p>portfolio</p>
+  return (
+    <>
+      <header>
+        <a href="#">logo</a>
+        <nav>
+          <ul>
+            <li>Обо мне</li>
+            <li>Навыки</li>
+            <li>Проекты</li>
+          </ul>
+        </nav>
+        <button type="button">sign in</button>
+      </header>
+      <main>dynamic background</main>
+      <section>Плеер</section>
+      <aside>Опрос c D3.js</aside>
+      <footer>2023</footer>
+    </>
+  )
 }
+
 export default App
