07e6e536e7e5197dd5e1414dfa52c958c31a1ce4
Arkadiy B <adiy.work.mail@ya.ru>
2023-01-21 17:05:34 +0300

    feat: decomposition app router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,7 @@
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './App'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />
-  }
-])
+import { RouterProvider } from 'react-router-dom'
+import { AppRouter } from './App/AppRouter'
 
 const root = document.getElementById('root') as HTMLElement
 
-ReactDOM.createRoot(root).render(<RouterProvider router={router} />)
+ReactDOM.createRoot(root).render(<RouterProvider router={AppRouter} fallbackElement={<>Загрузка...</>} />)
