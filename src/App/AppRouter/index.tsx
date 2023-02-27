7df6c932a8d66c137b88522f3093c4d836d15d1f
Arkadiy B <adiy.work.mail@ya.ru>
2023-02-27 16:03:09 +0300

    feat: add not found page

--- src/App/AppRouter/index.tsx
+++ src/App/AppRouter/index.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom'
 import App from '../../App'
+import NotFoundPage from '../../pages/NotFoundPage'
 
 export const AppRouter = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+  },
+  {
+    path: '*',
+    element: <NotFoundPage />,
   },
 ])
