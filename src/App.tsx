038b9d7ada05a8e9be941799af74960cc3e4a3c1
AdiyDev <adiy.personal.mail@ya.ru>
2023-03-07 01:06:21 +0300

    feat(app): remove tmp header

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,6 @@ import { MantineProvider } from '@mantine/core'
 import { Notifications } from '@mantine/notifications'
 import MainLayout from './layouts/MainLayout'
 
-const Header = styled.header({
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  background: '#2196f3',
-  padding: '0 2rem',
-})
-
 const MainContainer = styled.main({
   width: '100%',
   padding: '2rem 0',
@@ -47,22 +39,6 @@ const App = () => {
     >
       <Notifications />
       <MainLayout>
-        <Header>
-          <div>logo</div>
-          <nav>
-            <ul>
-              <li>Обо мне</li>
-              <li>Проекты</li>
-            </ul>
-          </nav>
-          <ul style={{ listStyle: 'square' }}>
-            <li>i18n</li>
-            <li>theme switch</li>
-            <li>
-              <button type="button">sign in</button>
-            </li>
-          </ul>
-        </Header>
         <MainContainer>
           <section>dynamic background</section>
           <section>Плеер</section>
